a050e537416ae6ad1d3b5a7376c10c1d085b42f9
James Davidson <jedavidson2000@gmail.com>
2021-01-01 00:52:22 +1100

    2013 will be my year.

--- js/blinkCursor.ts
+++ js/blinkCursor.ts
@@ -1,7 +1,7 @@
 (() => {
     const cursorDuration: number = 550;
     let cursorIsVisible: boolean = true;
-    
+
     setInterval(() => {
         const cursor: HTMLElement = document.getElementById('eol-cursor');
 
